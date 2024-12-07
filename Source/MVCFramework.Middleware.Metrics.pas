d79b088765f4a0a0d6532bfce801488939e26d0b
Marco Breveglieri <marcobreveglieri@users.noreply.github.com>
2024-12-07 18:34:26 +0100

    feat: new config record and http request duration
    + added a new configuration record to customize the behavior of the
    middleware;
    
    + added the ability to enable collection of HTTP request
    durations for the appropriate histogram metric (thanks to David Aspden
    a.k.a. @Tuxino for the idea and the initial code).

--- Source/MVCFramework.Middleware.Metrics.pas
+++ Source/MVCFramework.Middleware.Metrics.pas
@@ -5,6 +5,17 @@ interface
 uses
   MVCFramework;
 
+type
+
+{ TMVCMetricsMiddlewareConfig }
+
+  TMVCMetricsMiddlewareConfig = record
+    HttpRequestDurationEnabled: Boolean;
+    HttpRequestDurationCollector: string;
+    PathInfo: string;
+    constructor Create(const APathInfo: string);
+  end;
+
 { Routines }
 
 /// <summary>
@@ -12,14 +23,26 @@ uses
 ///  to export metric values from Prometheus Client.
 /// </summary>
 /// <param name="APathInfo">Set the path that will expose the metrics.</param>
-function GetMetricsMiddleware(const APathInfo: string = '/metrics'): IMVCMiddleware;
+function GetMetricsMiddleware(const APathInfo: string = '/metrics'): IMVCMiddleware; overload;
+
+/// <summary>
+///  Returns a new instance of DMVC middleware configured as specified
+///  to export metric values from Prometheus Client.
+/// </summary>
+/// <param name="AConfig">Specified the settings for this middleware.</param>
+function GetMetricsMiddleware(const AConfig: TMVCMetricsMiddlewareConfig): IMVCMiddleware; overload;
 
 implementation
 
 uses
-  System.Classes, System.SysUtils,
+  System.Classes,
+  System.DateUtils,
+  System.StrUtils,
+  System.SysUtils,
   MVCFramework.Commons,
-  Prometheus.Registry, Prometheus.Exposers.Text;
+  Prometheus.Collectors.Histogram,
+  Prometheus.Exposers.Text,
+  Prometheus.Registry;
 
 type
 
@@ -30,9 +53,11 @@ type
   /// </summary>
   TMetricsMiddleware = class(TInterfacedObject, IMVCMiddleware)
   private
-    FPathInfo: string;
+    FConfig: TMVCMetricsMiddlewareConfig;
+    procedure HttpRequestDurationHistogramAfterAction(AContext: TWebContext);
+    procedure HttpRequestDurationHistogramBeforeAction(AContext: TWebContext);
   public
-    constructor Create(const APathInfo: string);
+    constructor Create(const AConfig: TMVCMetricsMiddlewareConfig);
     procedure OnAfterControllerAction(Context: TWebContext;
       const AControllerQualifiedClassName: string; const AActionName: string;
       const AHandled: Boolean);
@@ -43,19 +68,61 @@ type
     procedure OnBeforeRouting(Context: TWebContext; var Handled: Boolean);
   end;
 
+{ TMVCMetricsMiddlewareConfig }
+
+constructor TMVCMetricsMiddlewareConfig.Create(const APathInfo: string);
+begin
+  PathInfo := IfThen(Length(APathInfo) > 0, APathInfo, '/metrics');
+  HttpRequestDurationEnabled := False;
+  HttpRequestDurationCollector := 'http_request_duration_seconds';
+end;
+
 { TMetricsMiddleware }
 
-constructor TMetricsMiddleware.Create(const APathInfo: string);
+constructor TMetricsMiddleware.Create(const AConfig: TMVCMetricsMiddlewareConfig);
 begin
   inherited Create;
-  FPathInfo := APathInfo;
+  FConfig := AConfig;
+end;
+
+procedure TMetricsMiddleware.HttpRequestDurationHistogramAfterAction(AContext: TWebContext);
+begin
+  // Get the request start date/time value as raw string from context data.
+  var LTimeStampRaw: string;
+  if not AContext.Data.TryGetValue('RequestStartTimeStamp', LTimeStampRaw) then
+    Exit;
+  // Convert the raw value from string to an effective date/time value.
+  var LTimeStamp := StrToFloat(LTimeStampRaw);
+  // Determine the request duration in seconds calling the appropriate function.
+  var LDuration := SecondSpan(Now, TDateTime(LTimeStamp));
+  // Find the appropriate histogram collector to store metric values.
+  var LCollector := TCollectorRegistry.DefaultRegistry
+    .GetCollector<THistogram>(FConfig.HttpRequestDurationCollector);
+  // Register a histogram metric with default buckets and labels if not found.
+  if not Assigned(LCollector) then
+  begin
+    LCollector := THistogram.Create(FConfig.HttpRequestDurationCollector,
+        'Time taken to process request in seconds', [], ['path', 'status']);
+    LCollector.Register();
+  end;
+  // Add the current duration to the histogram metric collector.
+  LCollector
+    .Labels([AContext.Request.PathInfo, IntToStr(AContext.Response.StatusCode)])
+    .Observe(LDuration);
+end;
+
+procedure TMetricsMiddleware.HttpRequestDurationHistogramBeforeAction(AContext: TWebContext);
+begin
+  // Store the request start date/time to feed histogram metrics.
+  AContext.Data['RequestStartTimeStamp'] := FloatToStr(Extended(Now));
 end;
 
 procedure TMetricsMiddleware.OnAfterControllerAction(Context: TWebContext;
   const AControllerQualifiedClassName, AActionName: string;
   const AHandled: Boolean);
 begin
-  // Do nothing.
+  if FConfig.HttpRequestDurationEnabled then
+    HttpRequestDurationHistogramAfterAction(Context);
 end;
 
 procedure TMetricsMiddleware.OnAfterRouting(Context: TWebContext;
@@ -68,15 +135,15 @@ procedure TMetricsMiddleware.OnBeforeControllerAction(Context: TWebContext;
   const AControllerQualifiedClassName, AActionNAme: string;
   var Handled: Boolean);
 begin
-  // Do nothing.
+  if FConfig.HttpRequestDurationEnabled then
+    HttpRequestDurationHistogramBeforeAction(Context);
 end;
 
 procedure TMetricsMiddleware.OnBeforeRouting(Context: TWebContext;
   var Handled: Boolean);
 begin
-  Context.Data.AddOrSetValue('RequestStartTime', FloatToStr(Now));
   // Check whether the current path request matches the metrics one.
-  if not SameText(Context.Request.PathInfo, FPathInfo) then
+  if not SameText(Context.Request.PathInfo, FConfig.PathInfo) then
     Exit;
 
   // We create a stream that will contain metric values exposed as text,
@@ -110,7 +177,14 @@ end;
 
 function GetMetricsMiddleware(const APathInfo: string): IMVCMiddleware;
 begin
-  Result := TMetricsMiddleware.Create(APathInfo);
+  Result := TMetricsMiddleware.Create(
+    TMVCMetricsMiddlewareConfig.Create(APathInfo)
+  );
+end;
+
+function GetMetricsMiddleware(const AConfig: TMVCMetricsMiddlewareConfig): IMVCMiddleware; overload;
+begin
+  Result := TMetricsMiddleware.Create(AConfig);
 end;
 
 end.
